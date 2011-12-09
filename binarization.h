af2187d268139c4791cfcf36290276d79f6186f4
Ivan Batsuev <ivan@batsuev.com>
2011-12-09 17:31:45 +0900

    Добавлены файлы, в которых будут реализованы функции дискретизации и бинаризации вещественных данных. Написаны базовые наблюдения и рекомендации по реализации этих алгоритмов.

---
+++ binarization.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   binarization.h
+ * Author: spas
+ *
+ * Created on 9 Декабрь 2011 г., 13:13
+ */
+
+#ifndef BINARIZATION_H
+#define	BINARIZATION_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* BINARIZATION_H */
+
